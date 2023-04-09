b3dda7f40907e9b1fb741a615fd52683f2ffaaa0
Ruslana Striuchenko <dokova.ruslana@ukr.net>
2023-04-10 00:31:09 +0300

    change the time at setInterval from 5000 to 3000

--- js/level1.js
+++ js/level1.js
@@ -253,7 +253,7 @@ clickFight.addEventListener('click', function() {
 let idInterval = 0;
 idInterval = setInterval(function() {
   updateMainPlayerLives();
-}, 4000);
+}, 3000);
 
 window.addEventListener("keydown", (e) => {
   if (e.code === 'Space') {
@@ -265,7 +265,7 @@ window.addEventListener("keyup", (e) => {
   if (e.code === 'Space') {
     idInterval = setInterval(() => {
       updateMainPlayerLives();
-    }, 4000);
+    }, 3000);
   }
 });
 
