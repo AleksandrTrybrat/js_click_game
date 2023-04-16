5dad77eeccfe538b60c76f60a7556af760651e68
Ruslana Striuchenko <dokova.ruslana@ukr.net>
2023-04-17 01:14:03 +0300

    added back sound

--- js/level3.js
+++ js/level3.js
@@ -1,5 +1,10 @@
 'use strict';
-
+window.addEventListener('mousemove', () => {
+  const backSound = new Audio();
+  backSound.src = './audio/заднийФон.mp3';
+  backSound.play(); 
+  backSound.volume(0.5);
+  }, {once: true});
 
 // Эффект дождя
 function rain() {
